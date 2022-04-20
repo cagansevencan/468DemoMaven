1c5e1257c379822e5a2032c3ca8938ac0d50a4b4
Cagan Sevencan <36283663+cagansevencan@users.noreply.github.com>
2022-04-20 15:45:01 -0700

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO TBL_EMPLOYEES (first_name, last_name, email)
+ VALUES
+        ( 'DAN', 'vega', 'danvega@gmail.com'),
+        ( 'Jen', 'vega', 'jenvega@gmail.com'),
+        ( 'Bella', 'vega', 'bellavega@gmail.com');
